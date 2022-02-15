655d5c19fac0180c848a45e6911239cae83c8401
Serhii Melnykov <serhii.melnykov@outlook.com>
2022-02-15 23:32:50 +0200

    Выделение PHP версии в отдельный проект

---
+++ app/PDOInstance.php
@@ -0,0 +1,40 @@
+<?php 
+
+class PDOInstance {
+	
+	public static array $instances = [];
+	
+	static public function get(string $host, string $db, string $user, string $pass): PDO {
+		
+		$params = [$host, $db, $user];
+		$instances = &self::$instances;
+		$paramscount = count($params);
+		$last_index = $paramscount - 1;
+		
+		for ($i = 0; $i < $paramscount; $i++) {
+			$p = $params[$i];
+			// Погружение на уровень.
+			if (isset($instances[$p])) {
+				if ($i == $last_index) {
+					return $instances[$p];
+				}
+				$instances = &$instances[$p];
+			// иначе создается уровень.
+			} else {
+				if ($i == $last_index) {
+					$conn_options = sprintf(
+						"mysql:host=%s;dbname=%s;charset=utf8", 
+						$host,
+						$db
+					);	
+					$pdo = new PDO($conn_options, $user, $pass);
+					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_NUM);
+					return $instances[$p] = $pdo;
+				}
+				$instances[$p] = [];
+				$instances = &$instances[$p];
+			}
+		}
+	}
+}
---
+++ app/index.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+$start = new DateTime();
+
+require './FileDataInfo.php';
+require './PDOInstance.php';
+
+$credentials = file_get_contents('./.credentials.json') OR die("файл конфигурации не доступен");
+$file_config = json_decode($credentials)->configFile;
+
+// --- программа ---
+if (is_readable($file_config) && !is_dir($file_config)) {
+    $datafileinfo_set = [];
+	$files_params = json_decode(file_get_contents($file_config), true);
+    foreach ($files_params as $config) {
+		
+		$pdo = PDOInstance::get($config['host'], $config['database'], $config['user'], $config['pass']);
+
+        foreach ($config['tables'] as $table) {
+            foreach ($table['files'] as $file) {
+                $datafileinfo_set[] = new FileDataInfo(
+                    $pdo, $table['tablename'], $file['filename'], $file['startrow'], $file['columns']
+                );
+            }
+        }
+    }
+    foreach ($datafileinfo_set as $fileObject) {
+        $fileObject->run();
+    }
+    print "Завершено. Время: " . $start->diff(new DateTime())->format("h:%H m:%I s:%S ms:%f");
+} else {
+    print "Неудалось прочитать файл {$file_config}";
+}
